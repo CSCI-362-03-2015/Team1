da5eea5f75629cdf77c7363837c53987befa1fd1
brilliantcal <brillch@g.cofc.edu>
2015-09-03 11:38:00 -0400

    This is Callum's Third version of the script and it opens the file automatically, and creates an unordered list

--- myList.sh
+++ myList.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #This script displays the contents of a directory. written by themonkeycatfishcoalition
 touch test.html
-echo "<html><body>" >> test.html
+echo "<html><body><ul>" >> test.html
 ls>>test.html
-echo "</html></body>" >> test.html
+echo "</ul></body></html>" >> test.html
 see test.html
 exit 0
