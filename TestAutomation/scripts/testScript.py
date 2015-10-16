4229bd8a26556f36fa1a7eb2a5ee9063e437ba57
brilliantcal <brillch@g.cofc.edu>
2015-10-15 21:48:27 -0400

    the beginnings of the test framework

--- TestAutomation/scripts/testScript.py
+++ TestAutomation/scripts/testScript.py
@@ -2,12 +2,15 @@ import os
 import unittest
 import subprocess
 
-def main():
-
-    tests=[]
-    #this portion empties the temp directory 
+def delTemp():
+     #this portion empties the temp directory 
     for j in os.listdir("../temp/"):
         os.remove("../temp/" + j)
+
+def main():
+
+    delTemp()
+    tests=[]
     #changes to the TestAutomation directory
     os.chdir("../")
     for file in os.listdir("testCases"):
@@ -20,7 +23,9 @@ def main():
                     lines.append(line)
                     i = i+1
                 print("this is test case #: "+lines[0].strip('\n'))
+                tests.append(lines[0])
                 print("this will be "+lines[1].strip('\n'))
+                
             
 
 main()
