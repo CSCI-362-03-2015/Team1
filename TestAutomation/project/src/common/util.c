8e7847f7dbbd96d62c1fe3ae9ad2990f60052748
Stefan Moser <moserse@users.noreply.github.com>
2015-11-23 18:39:37 -0500

    Update util.c

--- TestAutomation/project/src/common/util.c
+++ TestAutomation/project/src/common/util.c
@@ -478,16 +478,14 @@ round_to_power_of_2(uint64_t u64)
   low = U64_LITERAL(1) << lg2;
 
   if (lg2 == 63)
-    /*return low; FAULT BELOW*/
-    return high;
+    return low; 
+
 
   high = U64_LITERAL(1) << (lg2+1);
   if (high - u64 < u64 - low)
-    /*return high; fault below*/
-    return low;
-  else
-    /*return low; fault below*/
     return high;
+  else
+    return low;
 }
 
 /** Return the lowest x such that x is at least <b>number</b>, and x modulo
